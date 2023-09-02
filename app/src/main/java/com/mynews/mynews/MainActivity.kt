acf58ab577b53d4aef8d493fcd1fe47389455be3
Bintina <105106238+Bintina@users.noreply.github.com>
2023-09-02 10:45:15 +0300

    TopStoriesFragmentImplemented
    Top Stories implemented as fragment

--- app/src/main/java/com/mynews/mynews/MainActivity.kt
+++ app/src/main/java/com/mynews/mynews/MainActivity.kt
@@ -2,32 +2,32 @@ package com.mynews.mynews
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import androidx.lifecycle.lifecycleScope
 import com.mynews.mynews.topstories.adapter.Adapter
 import com.mynews.mynews.topstories.data.Datasource
 import com.mynews.mynews.databinding.ActivityMainBinding
+import com.mynews.mynews.topstories.controller.TopStoriesFragment
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var adapter: Adapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
 
-        initialiseList()
-
-        lifecycleScope.launch(Dispatchers.IO) {
-            Datasource.loadNews(adapter)
+                setReorderingAllowed(true)
+                add<TopStoriesFragment>(R.id.fragment_container_view)
+            }
         }
     }
 
-    private fun initialiseList() {
-        adapter = Adapter()
-        binding.recyclerView.adapter = adapter
 }
-    }
